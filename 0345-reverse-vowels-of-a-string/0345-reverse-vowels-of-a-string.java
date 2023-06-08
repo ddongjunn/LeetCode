8e6e688d49d2640fec747d27dc6798b3dc06bc2f
dongjun lee <ddongjunn@naver.com>
2023-06-08 17:46:07 +0900

    Time: 6 ms (59.08%), Space: 43.8 MB (52.58%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String reverseVowels(String word) {
-		Set<Character> vowel = new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
+		Set<Character> vowels = new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
 		int start = 0;
 		int end = word.length() - 1;
 		boolean startFlag = false;
@@ -8,38 +8,26 @@ class Solution {
 		
 		char[] ch = word.toCharArray();
 		
-		while(start < end) {
-			if(!vowel.contains(ch[start])) {	
-				start++;
-			}else {
-				startFlag = true;
-			}
-			
-			if(!vowel.contains(ch[end])) {
-				end--;
-			}else {
-				endFlag = true;
-			}
-			
-			if(startFlag && endFlag) {
-				swap(ch, start, end);
-				start++;
-				end--;
-				startFlag = false;
-				endFlag = false;
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<ch.length; i++) {
-			sb.append(ch[i]);
-		}
-		return sb.toString();
+		while (start < end) {
+            char startChar = ch[start];
+            char endChar = ch[end];
+
+            if (!vowels.contains(startChar)) {
+                start++;
+            } else if (!vowels.contains(endChar)) {
+                end--;
+            } else {
+                swap(ch, start, end);
+                start++;
+                end--;
+            }
+        }
+		return new String(ch);
     }
 	
 	public void swap(char[] ch, int x, int y) {
-		char c = ch[x];
+		char temp = ch[x];
 		ch[x] = ch[y];
-		ch[y] = c;
+		ch[y] = temp;
 	}
 }
