0ef06874630449cf3e6c9a2c98231af5806a7099
dongjun lee <ddongjunn@naver.com>
2023-06-13 12:31:18 +0900

    Time: 14 ms (13.86%), Space: 45.4 MB (16.31%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+		int pos = 0;
+		for(int i=0; i< nums.length; i++) {
+			if(nums[i] != 0) {
+				if(i != pos) {
+					nums[pos] = nums[i];
+					nums[i] = 0;
+				}
+				pos++;
+			}
+		}
+		System.out.println(Arrays.toString(nums));
+	}
+}
