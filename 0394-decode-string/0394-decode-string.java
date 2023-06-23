53fbe67940f5cacb8fdecd0001313c29f682c6a1
dongjun lee <ddongjunn@naver.com>
2023-06-23 13:39:45 +0900

    Time: 1 ms (76.42%), Space: 40.7 MB (59.78%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String s) {
+		Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        
+        for(int i=0; i<s.length(); i++) {
+        	char ch = s.charAt(i);
+        	if(Character.isDigit(ch)) {
+        		int cnt = 0;
+        		while(Character.isDigit(s.charAt(i))) {
+        			cnt = 10 * cnt + (s.charAt(i) - '0');
+        			i++;
+        		}
+        		countStack.push(cnt);
+        		i--;
+        		continue;
+        	}else if (ch == '[') {
+        		resStack.push(result.toString());
+        		result.setLength(0);
+        	}else if (ch == ']') {
+        		StringBuilder temp = new StringBuilder(resStack.pop());
+        		int cnt = countStack.pop();
+        		for(int j=0; j<cnt; j++) {
+        			temp.append(result);
+        		}
+        		result = temp;
+        	}else {
+        		result.append(s.charAt(i));
+        	}
+        }		
+
+		return result.toString();
+	}
+}
