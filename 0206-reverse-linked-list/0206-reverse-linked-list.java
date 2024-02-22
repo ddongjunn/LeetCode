a8c1140c694b6e0b492c6e2ecb650c76b6906c85
dongjun lee <ddongjunn@naver.com>
2024-02-22 22:26:13 +0900

    Time: 0 ms (100.00%), Space: 42.2 MB (72.06%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,23 +10,17 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        
-        ListNode prev = null;
-        ListNode node = head;
-        while(node != null){
-            // 현재 노드의 다음 노드 미리 지정
-            ListNode next = node.next;
-            
-            // 현재 노드의 다음으로 이전 노드 지정
-            node.next = prev;
-            
-            // 이전 노드는 현재 노드로 지정
-            prev = node;
-            
-            // 미리 지정했던 다음 노드를 현재 노드로 변경
-            node = next;
+        return reverse(head, null);
+    }
+    
+    public ListNode reverse(ListNode node, ListNode prev){
+        if(node == null){
+            return prev;
         }
         
-        return prev;
+        ListNode next = node.next;
+        node.next = prev;
+        
+        return reverse(next, node);
     }
 }
