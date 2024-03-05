3e8226a116175445e35bb6e9d711d94006beaa32
dongjun lee <ddongjunn@naver.com>
2024-03-06 00:20:22 +0900

    Time: 2 ms (54.62%), Space: 41.2 MB (81.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,21 @@
 class Solution {
-    public boolean isValid(String s) {
-        Map<Character, Character> table = new HashMap<>(){{
-            put(')','(');
-            put('}','{');
-            put(']','[');
-        }};
-        
+    
+    private static final Map<Character, Character> PAIR_MAPPING = Map.of(')','(','}','{',']','[');
+    
+    public boolean isValid(String s) {    
         Deque<Character> stack = new ArrayDeque<>();
+        
         for(char ch : s.toCharArray()){
-            if(!table.containsKey(ch)){
+            if(!PAIR_MAPPING.containsKey(ch)){
                 stack.push(ch);
                 continue;
-            } 
+            }    
             
-            if(stack.isEmpty() || table.get(ch) != stack.pop()){
+            if(stack.isEmpty() || PAIR_MAPPING.get(ch) != stack.pop()){
                 return false;
             }
         }
         
-        return stack.size() == 0;
-    }
+        return stack.isEmpty();
+    }       
 }
