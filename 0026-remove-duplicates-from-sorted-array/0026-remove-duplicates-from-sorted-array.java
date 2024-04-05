59131734f229956d27a66354f193e7b1f9713eca
dongjun lee <ddongjunn@naver.com>
2024-04-05 16:57:45 +0900

    Time: 0 ms (100.00%), Space: 44.5 MB (71.50%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,19 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new TreeSet<>();
-        for(int i = 0; i < nums.length; i++){
-            if(set.contains(nums[i])){
-                continue;
+        int j = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i-1] != nums[i]){
+                nums[j++] = nums[i];
             }
-            set.add(nums[i]);
-        }
-
-        int idx = 0;
-        Iterator iter = set.iterator();
-        while(iter.hasNext()){
-            nums[idx++] = (int) iter.next();
-        }
-        
-        return set.size();
+        } 
+        return j;
     }
 }
