602f2742fe56582318d1dbf41975ee6906956d71
dongjun lee <ddongjunn@naver.com>
2024-04-09 10:07:37 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (47.55%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -2,16 +2,11 @@ class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int time = 0;
         
-        while(tickets[k] > 0){
-            for(int i = 0; i < tickets.length; i++){
-                if(tickets[i] > 0){
-                    tickets[i]--;
-                    time++;
-                }
-                
-                if(tickets[k] == 0){
-                    return time;
-                }
+        for(int i = 0; i < tickets.length; i++){
+            if(i <= k){
+                time += Math.min(tickets[i], tickets[k]);
+            }else{
+                time += Math.min(tickets[i], tickets[k] - 1);
             }
         }
         
