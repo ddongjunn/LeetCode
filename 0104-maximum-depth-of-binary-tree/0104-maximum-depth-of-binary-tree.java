27562e41945d4481731809a895ed6e00d01fd2ed
dongjun lee <ddongjunn@naver.com>
2024-06-04 09:51:37 +0900

    Time: 0 ms (100.00%), Space: 42.4 MB (67.49%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,13 +15,10 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null){
+        if(root == null) {
             return 0;
         }
         
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        
-        return Math.max(left, right) + 1;
+        return Math.max(maxDepth(root.right), maxDepth(root.left)) + 1;
     }
 }
