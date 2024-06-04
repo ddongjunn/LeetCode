bc23be5fce63ea967b3d0569f3b68ccae2ae5dd7
dongjun lee <ddongjunn@naver.com>
2024-06-04 10:46:51 +0900

    Time: 8 ms (47.82%), Space: 50.2 MB (20.59%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -15,23 +15,36 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        dfs(root.left, root.right, 1);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int level = 0;
+        
+        while(!q.isEmpty()) {
+            
+            int size = q.size();
+            while(size-- > 0) {
+                TreeNode node = q.poll();
+                if(node.left != null && node.right != null) {
+                    q.offer(node.left);
+                    q.offer(node.right);
+                }
+            }
+            
+            level++;
+            if(level % 2 == 1 && !q.isEmpty()) {
+                int i = 0;
+                int[] nums = new int[q.size()];
+                for(TreeNode node : q) {
+                    nums[i++] = node.val;
+                }
+                
+                int j = q.size() - 1;
+                for(TreeNode node : q) {
+                    node.val = nums[j--];
+                }
+            }
+        }
+        
         return root;
     }
-    
-    public void dfs(TreeNode node1, TreeNode node2, int level) {
-        if(node1 == null && node2 == null) {
-            return;
-        }
-        
-        if(level % 2 == 1) {
-            int tmp = node1.val;
-            node1.val = node2.val;
-            node2.val = tmp;
-        }
-        
-        dfs(node1.left, node2.right, level + 1);
-        dfs(node1.right, node2.left, level + 1);
-    }
-
 }
