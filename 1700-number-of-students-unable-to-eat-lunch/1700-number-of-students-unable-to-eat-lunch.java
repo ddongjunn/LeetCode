cdbde1abfcdf4bf4a4932cab96566e83ce468f31
dongjun lee <ddongjunn@naver.com>
2024-06-23 00:07:44 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (20.81%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,26 +1,17 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> student = new ArrayDeque();
-        for(int i : students) {
-            student.offer(i);
+        int[] count = new int[2];
+        for (int student : students) {
+            count[student]++;
         }
         
-        Queue<Integer> sandwich = new ArrayDeque();
-        for(int i : sandwiches) {
-            sandwich.offer(i);
+        for (int sandwich : sandwiches) {
+            if (count[sandwich] == 0) {
+                break;
+            }
+            count[sandwich]--;
         }
         
-        int lastServed = 0;
-        while(!student.isEmpty() && lastServed < student.size()) {
-            if(student.peek() == sandwich.peek()) {
-                student.poll();
-                sandwich.poll();
-                lastServed = 0;
-            } else {
-                student.offer(student.poll());
-                lastServed++;
-            }  
-        }
-        return student.size();
+        return count[0] + count[1];
     }
 }
