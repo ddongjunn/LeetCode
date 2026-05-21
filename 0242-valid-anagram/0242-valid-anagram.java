97bb29018d3f3ec06f5bced7812fbba87077e5ce
dongjun lee <ddongjunn@naver.com>
2026-05-21 15:45:37 +0900

    Time: 5 ms (62.4%), Space: 44.3 MB (82.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+        int sLen = s.length();
+        int tLen = t.length();
+        if (sLen != tLen) {
             return false;
         }
-        
+
         int idx[] = new int[26];
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < sLen; i++) {
             idx[s.charAt(i) - 'a']++;
             idx[t.charAt(i) - 'a']--;
         }
 
-        return Arrays.stream(idx).allMatch(i -> i == 0);
+        for (int i = 0; i < idx.length; i++) {
+            if (idx[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
