2316162b36a8f770d61d311405045b1a338642ab
dongjun lee <ddongjunn@gmail.com>
2026-07-10 13:10:57 +0900

    Time: 20 ms (83.82%), Space: 56.3 MB (76.05%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,28 +1,17 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-
+        
         int left = 0;
-        int right = people.length -1;
+        int right = people.length - 1;
         int answer = 0;
-        boolean carry = false;
-
         while (left <= right) {
-            int l = people[left];
-            int r = people[right];
-
-            if (l + r > limit) {
-                answer++;
-                right--;
-            } else if (l + r == limit || l + r < limit) {
-                answer++;
-                right--;
-                left++;
-            } else {
-                answer++;
+            if (people[left] + people[right] <= limit) {
                 left++;
             }
+            right--;
+            answer++;
         }
-        return answer; 
+        return answer;
     }
 }
