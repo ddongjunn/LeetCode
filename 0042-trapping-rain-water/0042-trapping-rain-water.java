00f7d4883a0b432c819e0e1fb44d690a7f27ffa7
dongjun lee <ddongjunn@gmail.com>
2026-07-11 17:22:09 +0900

    Time: 1 ms (60.39%), Space: 48.6 MB (22.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int trap(int[] height) {
         int answer = 0;
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
-        
-        while(left < right){
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
-            
-            if(leftMax <= rightMax){
-                answer += leftMax - height[left];
-                left++;
-            }else{
-                answer += rightMax - height[right];
-                right--;
-            }
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+
+        leftMax[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
-        
+
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        for (int i = 0; i < height.length; i++) {       
+            answer += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
         return answer;
     }
 }
