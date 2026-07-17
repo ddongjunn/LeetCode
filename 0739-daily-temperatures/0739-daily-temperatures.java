bde080c8df95d7d491809dbaf4e709833a8f66af
jay <ddongjunn@gmail.com>
2026-07-17 20:16:56 +0900

    Time: 22 ms (96.77%), Space: 103.7 MB (86.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -2,20 +2,14 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] ans = new int[temperatures.length];
         var stack = new ArrayDeque<Integer>();
-        var map = new HashMap<Integer, Integer>();
 
         for (int i = 0; i < temperatures.length; i++) {
             while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
                 int idx = stack.pop();
-                map.put(idx, i - idx);
+                ans[idx] = i - idx; 
             }   
             stack.push(i);
         }
-        
-        for (int i = 0; i < temperatures.length; i++) {
-            ans[i] = map.getOrDefault(i, 0);
-        }
-
         return ans;
     }
 } 
