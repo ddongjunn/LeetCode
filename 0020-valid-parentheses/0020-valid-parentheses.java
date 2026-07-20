f6aa66dd822adeba24bf54947952fb19b2b3d9e0
jay <ddongjunn@gmail.com>
2026-07-20 17:20:09 +0900

    Time: 3 ms (86.86%), Space: 43.4 MB (41.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        Map<Character, Character> pair = Map.of(
-            ')', '(',
-            '}', '{',
-            ']', '['
-        );
+        var map = Map.of('(',')','[',']','{','}');
+        var stack = new ArrayDeque<Character>();
 
-        Deque<Character> stack = new ArrayDeque<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch ==  '{' || ch == '[') {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
                 continue;
             }
 
-            if (stack.isEmpty() || pair.get(ch) != stack.pop()) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            if (map.get(stack.pop()) != ch) {
                 return false;
             }
         }
