169d3cfb58c452c38ce4b74ffbe9f55a7d0b43f4
jay <ddongjunn@gmail.com>
2026-07-21 23:07:18 +0900

    Time: 45 ms (26.39%), Space: 46.8 MB (84.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        var map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            int need = target - nums[i];
-            Integer prev = map.get(need);
-            if (prev != null) {
-                return new int[]{prev, i};
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
             }
-            
-            map.put(nums[i] , i);
         }
-
-        throw new IllegalArgumentException();
+        return new int[]{};
     }
 }
