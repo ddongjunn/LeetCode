ab2b4973a7c6955a6b60f20898597445a4922d86
jay <ddongjunn@gmail.com>
2026-07-24 10:28:59 +0900

    Time: 0 ms (100%), Space: 44.7 MB (66.95%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,10 +5,6 @@ class Solution {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            }
-
             if (nums[mid] < target) {
                 left = mid + 1;
             } else {
