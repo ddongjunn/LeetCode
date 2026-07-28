61caaf58f1f8dda0e19aca93704bb12d128cb516
jay <ddongjunn@gmail.com>
2026-07-28 13:05:38 +0900

    Time: 2251 ms (5.25%), Space: 76.4 MB (5.74%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,13 +1,41 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int n : nums) {
-            pq.offer(n);
+        int targetIdx = nums.length - k;
+        int left = 0;
+        int right = nums.length - 1;
 
-            if (pq.size() > k) {
-                pq.poll();
+        while (left <= right) {
+            int pivotIdx = partition(nums, left, right);
+
+            if (pivotIdx == targetIdx) {
+                return nums[pivotIdx];
+            } else if (pivotIdx < targetIdx) {
+                left = pivotIdx + 1;
+            } else {
+                right = pivotIdx - 1;
             }
         }
-        return pq.peek();
+
+        throw new IllegalArgumentException();
+    }
+
+    public int partition (int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int smallIdx = left;
+
+        for (int current = left; current < right; current++) {
+            if (nums[current] < pivot) {
+                swap(nums, smallIdx, current);
+                smallIdx++;
+            }
+        }
+        swap (nums, smallIdx, right);
+        return smallIdx;
+    }
+
+    public void swap (int[] nums, int x, int y) {
+        int swap = nums[x];
+        nums[x] = nums[y];
+        nums[y] = swap;
     }
 }
