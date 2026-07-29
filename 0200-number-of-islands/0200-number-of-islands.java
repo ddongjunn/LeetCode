4e40d89dce878767c03da8424c835bb0e8556d55
jay <code.dongjunlee@gmail.com>
2026-07-29 12:39:40 +0900

    Time: 6 ms (20.69%), Space: 51.6 MB (83.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,57 +1,43 @@
 class Solution {
-    private final int[] DR = {1, 0, -1, 0};
-    private final int[] DC = {0, -1, 0, 1};
+    private final int[] DX = {1, 0, -1, 0};
+    private final int[] DY = {0, -1, 0, 1};
 
     public int numIslands(char[][] grid) {
         int rows = grid.length;
-        int cols = grid[0].length; 
-        int ans = 0;
+        int cols = grid[0].length;
 
         boolean[][] isVisited = new boolean[rows][cols];
+        int answer = 0;
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                if (grid[row][col] == '0' || isVisited[row][col]) {
-                    continue;
-                }
-
-                ans++;
-                bfs(grid, isVisited, row, col);
+                if (isVisited[row][col] || grid[row][col] == '0') continue;
+                bfs(row, col, grid, isVisited);
+                answer++;
             }
         }
-        return ans;
+
+        return answer;
     }
 
-    private void bfs (char[][] grid, boolean[][] isVisited, int row, int col) {
-        Queue<Pos> queue = new ArrayDeque<>();
-        queue.offer(new Pos(row, col));
-        isVisited[row][col] = true;
+    private void bfs(int row, int col, char[][] grid, boolean[][] isVisited) {
+        Queue<Pos> q = new ArrayDeque<>();
+        q.offer(new Pos(row, col));
 
-        while (!queue.isEmpty()) {
-            Pos current = queue.poll();
-            int x = current.x();
-            int y = current.y();
+        while (!q.isEmpty()) {
+            Pos pos = q.poll();
 
-            for (int dir = 0; dir < DR.length; dir++) {
-                int nx = x + DR[dir];
-                int ny = y + DC[dir];
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = pos.x() + DX[dir];
+                int ny = pos.y() + DY[dir];
 
-                if (nx < 0 || nx >= grid.length || ny < 0 || ny >= grid[0].length) {
-                    continue;
-                }
-
-                if (isVisited[nx][ny]) {
-                    continue;
-                }
-
-                if (grid[nx][ny] == '0') {
-                    continue;
-                }
+                if (nx < 0 || nx >= grid.length || ny < 0 || ny >= grid[0].length) continue;
+                if (isVisited[nx][ny] || grid[nx][ny] == '0') continue;
 
                 isVisited[nx][ny] = true;
-                queue.offer(new Pos(nx, ny));
+                q.offer(new Pos(nx, ny));
             }
         }
     }
 }
 
-record Pos (int x, int y){}
+record Pos (int x, int y) {}
