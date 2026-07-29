8cd1eb47db031a86385eb3c62f880bf4a9c85ae1
jay <code.dongjunlee@gmail.com>
2026-07-29 12:45:26 +0900

    Time: 3 ms (87.64%), Space: 52.5 MB (33.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -6,38 +6,23 @@ class Solution {
         int rows = grid.length;
         int cols = grid[0].length;
 
-        boolean[][] isVisited = new boolean[rows][cols];
         int answer = 0;
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                if (isVisited[row][col] || grid[row][col] == '0') continue;
-                bfs(row, col, grid, isVisited);
+                if (grid[row][col] == '0') continue;
+                dfs(grid, row, col);
                 answer++;
             }
         }
-
         return answer;
     }
 
-    private void bfs(int row, int col, char[][] grid, boolean[][] isVisited) {
-        Queue<Pos> q = new ArrayDeque<>();
-        q.offer(new Pos(row, col));
-
-        while (!q.isEmpty()) {
-            Pos pos = q.poll();
-
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = pos.x() + DX[dir];
-                int ny = pos.y() + DY[dir];
-
-                if (nx < 0 || nx >= grid.length || ny < 0 || ny >= grid[0].length) continue;
-                if (isVisited[nx][ny] || grid[nx][ny] == '0') continue;
-
-                isVisited[nx][ny] = true;
-                q.offer(new Pos(nx, ny));
-            }
-        }
+    private void dfs (char[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0') return;
+        grid[row][col] = '0';
+        dfs(grid, row + 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col - 1);
     }
 }
-
-record Pos (int x, int y) {}
