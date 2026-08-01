5a4063a09ba5c7cd6fc4e4036f7e2ea114562306
jay <code.dongjunlee@gmail.com>
2026-08-01 13:31:04 +0900

    Time: 1 ms (75.61%), Space: 43.4 MB (32.93%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.java
+++ 0722-remove-comments/0722-remove-comments.java
@@ -1,41 +1,43 @@
 class Solution {
     public List<String> removeComments(String[] source) {
         List<String> ans = new ArrayList<>();
-        boolean isBlocked = false;
         StringBuilder sb = new StringBuilder();
 
-        for (String str : source) {
-            for (int i = 0; i < str.length(); i++) {
-                char ch = str.charAt(i);
-                if (isBlocked) {
-                    if (ch == '*' && str.charAt(i + 1) == '/') {
-                        isBlocked = false;
+        boolean isBlock = false;
+
+        for (String cmd : source) {
+            for (int i = 0; i < cmd.length(); i++) {
+                char curr = cmd.charAt(i);
+                char next = i + 1 < cmd.length() 
+                        ? cmd.charAt(i + 1) 
+                        : ' ';
+
+                if (isBlock) {
+                    if (curr == '*' && next == '/') {
+                        isBlock = false;
                         i++;
-                    } 
-                    continue;
-                } else {
-                    if (ch == '/') {
-                        if (i + 1 < str.length() && str.charAt(i + 1) == '*') {
-                           isBlocked = true;
-                           i++;
-                           continue;
-                        } else if (i + 1 < str.length() && str.charAt(i + 1) == '/') {
-                            break;
-                        }
                     }
+                    continue;
                 }
 
-                if (!isBlocked) {
-                    sb.append(ch);
+                if (curr == '/' && next == '*') {
+                    isBlock = true;
+                    i++;
+                    continue;
                 }
+
+                if (curr == '/' && next == '/') {
+                    break;
+                }
+
+                sb.append(curr);
             }
 
-            if (!isBlocked && sb.length() > 0) {
+            if (!isBlock && sb.length() > 0) {
                 ans.add(sb.toString());
                 sb.setLength(0);
             }
         }
-
         return ans;
     }
 }
