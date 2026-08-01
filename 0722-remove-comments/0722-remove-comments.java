4662c72f93507174b5952f6c165874008c4e457a
jay <code.dongjunlee@gmail.com>
2026-08-01 13:16:27 +0900

    Time: 1 ms (75.61%), Space: 43.4 MB (32.93%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.java
+++ 0722-remove-comments/0722-remove-comments.java
@@ -1,44 +1,41 @@
 class Solution {
     public List<String> removeComments(String[] source) {
         List<String> ans = new ArrayList<>();
-        boolean isBlock = false;
+        boolean isBlocked = false;
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < source.length; i++) {
-            for (int j = 0; j < source[i].length(); j++) {
-                char current = source[i].charAt(j);
-                char next = j + 1 < source[i].length()
-                                ? source[i].charAt(j + 1)
-                                : '\0';
-                //end block
-                if (isBlock) {
-                    if (current == '*' && next == '/') {
-                        isBlock = false;
-                        j++;
+        for (String str : source) {
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                if (isBlocked) {
+                    if (ch == '*' && str.charAt(i + 1) == '/') {
+                        isBlocked = false;
+                        i++;
+                    } 
+                    continue;
+                } else {
+                    if (ch == '/') {
+                        if (i + 1 < str.length() && str.charAt(i + 1) == '*') {
+                           isBlocked = true;
+                           i++;
+                           continue;
+                        } else if (i + 1 < str.length() && str.charAt(i + 1) == '/') {
+                            break;
+                        }
                     }
-                    continue;
                 }
 
-                //start block //
-                if (current == '/' && next == '/') {
-                    break;
-                } 
-
-                //start block /*
-                if (current == '/' && next == '*') {
-                    isBlock = true;
-                    j++;
-                    continue;
+                if (!isBlocked) {
+                    sb.append(ch);
                 }
-
-                sb.append(current);
             }
 
-            if (!isBlock && sb.length() > 0) {
+            if (!isBlocked && sb.length() > 0) {
                 ans.add(sb.toString());
                 sb.setLength(0);
             }
         }
+
         return ans;
     }
 }
