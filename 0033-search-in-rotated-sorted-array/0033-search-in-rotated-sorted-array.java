e3905ea4117832076fb69ae1fc6dd9337f372bcc
jay <code.dongjunlee@gmail.com>
2026-08-01 14:50:17 +0900

    Time: 0 ms (100%), Space: 43.8 MB (47.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,10 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // 정렬된 지점 찾기
+            if (nums[left] <= nums[mid]) {
+                // left ~ mid 사이 target이 있으면 right 버리기
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;                   
+                } else {
+                    left = mid + 1;
+                }
+            } else { // mid ~ right 사이 target이 있으면 left 버리기
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
+
         return -1;
     }
 }
