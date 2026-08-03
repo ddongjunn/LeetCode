dbb08bbf9d7eca354d4d42710281a6c7495fd01b
jay <code.dongjunlee@gmail.com>
2026-08-03 15:04:30 +0900

    Time: 26 ms (99.62%), Space: 59.5 MB (24.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,10 +3,12 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> answer = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;
             if (i > 0 && nums[i - 1] == nums[i]) continue;
             
             int left = i + 1;
             int right = nums.length - 1;
+
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
