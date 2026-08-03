114f99bff141a20d7908bf7767b8371a9132f956
jay <code.dongjunlee@gmail.com>
2026-08-03 13:01:47 +0900

    Time: 96 ms (38.22%), Space: 69.1 MB (38.03%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -1,42 +1,42 @@
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-        Set<Pos> obstacleSet = new HashSet<>();
-        for (int[] obstacle : obstacles) {
-            obstacleSet.add(new Pos(obstacle[0], obstacle[1]));
+        Set<String> obstacle = new HashSet<>();
+        for (int i = 0; i < obstacles.length; i++) {
+            obstacle.add(obstacles[i][0] + ":" + obstacles[i][1]);
         }
+        
+        int[][] dirs = {
+            {0, 1},
+            {1, 0},
+            {0, -1},
+            {-1, 0},
+        };
 
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int maxDistance = 0;
         int dir = 0;
         int x = 0;
         int y = 0;
-        int maxDistance = 0;
         for (int cmd : commands) {
+
             if (-1 == cmd) {
                 dir = (dir + 1) % 4;
-                continue;
-            }
-
-            if (-2 == cmd) {
+            } else if (-2 == cmd) {
                 dir = (dir + 3) % 4;
-                continue;
-            }
+            } else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dirs[dir][0];
+                    int ny = y + dirs[dir][1];
 
-            for (int step = 0; step < cmd; step++) {
-                int nx = x + dirs[dir][0];
-                int ny = y + dirs[dir][1];
+                    if (obstacle.contains(nx+":"+ny)) {
+                        break;
+                    }
 
-                if (obstacleSet.contains(new Pos(nx, ny))) {
-                    break;
+                    x = nx;
+                    y = ny;
+                    maxDistance = Math.max(maxDistance, (x * x) + (y * y));
                 }
-
-                x = nx;
-                y = ny;
             }
-
-            maxDistance = Math.max(maxDistance, (int) ((Math.pow(x, 2)) + (Math.pow(y, 2))));
         }
         return maxDistance;
     }
 }
-
-record Pos (int x, int y) {}
