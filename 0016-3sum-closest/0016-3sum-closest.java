d4dac8969bde1efd4580287a57aac0eeee9d553b
jay <code.dongjunlee@gmail.com>
2026-08-03 15:35:51 +0900

    Time: 18 ms (40.77%), Space: 45.6 MB (49.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,7 +3,7 @@ class Solution {
         //target과 제일 가까운 sum
         Arrays.sort(nums);
         int closestSum = nums[0] + nums[1] + nums[2];
-        
+
         for (int i = 0; i < nums.length; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;;
 
@@ -11,6 +11,10 @@ class Solution {
             int right = nums.length - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
+                if (sum == target) {
+                    return sum;
+                }
+
                 if (Math.abs(sum - target) < Math.abs(closestSum - target)){
                     closestSum = sum;
                 }
@@ -21,7 +25,7 @@ class Solution {
                     right--;
                 } else {
                     while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[left] == nums[right - 1]) right--;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
                 }
