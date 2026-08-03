2a1fd63075bf92ab5ecc58a7e555f1de313494b4
jay <code.dongjunlee@gmail.com>
2026-08-03 14:05:39 +0900

    Time: 0 ms (100%), Space: 48.3 MB (32.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{findStart(nums, target), findEnd(nums, target)};
+        return new int[]{start(nums, target), end(nums, target)};
     }
 
-    public int findStart(int[] nums, int target) {
+    public int start(int[] nums, int target) {
+        int find = -1;
         int left = 0;
         int right = nums.length - 1;
-        int idx = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
+            if (nums[mid] == target) {
+                find = mid;
                 right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                idx = mid;
                 right = mid - 1;
             }
         }
-        return idx;
+        return find;
     }
 
-    public int findEnd(int[] nums, int target) {
+    public int end(int[] nums, int target) {
+        int find = -1;
         int left = 0;
         int right = nums.length - 1;
-        int idx = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
+            if (nums[mid] == target) {
+                find = mid;
                 left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                idx = mid;
-                left = mid + 1;
+                right = mid - 1;
             }
         }
-        return idx;
+        return find;
     }
 }
