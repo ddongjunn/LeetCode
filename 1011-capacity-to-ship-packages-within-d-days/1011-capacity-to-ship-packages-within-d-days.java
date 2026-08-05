3c1682f9d5a015c51b82073e1d26dce7d71d8138
jay <code.dongjunlee@gmail.com>
2026-08-05 17:36:07 +0900

    Time: 16 ms (10.15%), Space: 50 MB (88.59%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,33 +1,36 @@
-import java.util.Arrays;
-
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {        
+    public int shipWithinDays(int[] weights, int days) {
         int left = Arrays.stream(weights).max().getAsInt();
         int right = Arrays.stream(weights).sum();
-        
-        while(left < right) {
-            int mid = left + (right - left) / 2;            
-            if(condition(weights, mid, days)) {
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            int count = 0;
+            int countDay = 1;
+            for (int i = 0; i < weights.length; i++) {
+                if (count + weights[i] > mid) {
+                    count = 0;
+                    countDay++;
+                }
+
+                count += weights[i];
+            }
+            
+            if (countDay <= days) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
-        }        
+        }
         return left;
     }
-    
-    public boolean condition(int[] weights, int capacity, int days) {
-        int daysNeeded = 1;
-        int currentWeight = 0;
-        
-        for(int weight : weights) {
-            if(currentWeight + weight > capacity) {
-                daysNeeded++;
-                currentWeight = 0;
-            }
-            currentWeight += weight;
-        }
-        
-        return daysNeeded <= days;
-    }
 }
+
+/*
+    left -> 최소 후보 (가장 무거운 상자)
+    right -> 최대 후보
+
+    days 안에 선박의 최소 적재 용량
+    - 정렬 X
+*/
