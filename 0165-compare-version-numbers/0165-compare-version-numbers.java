907b1d8e936482f612a88071bd601d20fdda8f58
jay <code.dongjunlee@gmail.com>
2026-08-11 15:56:26 +0900

    Time: 1 ms (75.89%), Space: 43.1 MB (24.09%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,20 +1,40 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1s = version1.split("\\.");
-        String[] v2s = version2.split("\\.");
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int len = Math.max(v1.length, v2.length);
 
-        int len = Math.max(v1s.length, v2s.length);
-        for (int i = 0; i < len; i++) {
-            int v1 = v1s.length > i ? Integer.parseInt(v1s[i]) : 0;
-            int v2 = v2s.length > i ? Integer.parseInt(v2s[i]) : 0;
-
-            if (v1 > v2) {
-                return 1;
-            } else if (v1 < v2){
-                return -1;
+        int i = 0;
+        int j = 0;
+        while (i <= len && j <= len) {
+            int num1 = 0;
+            if (v1.length > i) {
+                num1 = Integer.parseInt(v1[i]);
             }
-        }
 
+            int num2 = 0;
+            if (v2.length > j) {
+                num2 = Integer.parseInt(v2[j]);
+            }
+
+            if (num1 < num2) {
+                return -1;
+            } else if (num1 > num2) {
+                return 1;
+            }
+            i++;
+            j++;
+        }
+        
         return 0;
     }
 }
+
+
+/**
+If version1 < version2, return -1.
+If version1 > version2, return 1.
+
+앞의 0은 무시한다
+둘중 하나 최대값으로 반복문 -> 길이가 짤리는 경우 0으로 채운다.
+ */
