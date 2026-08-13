84e88d9844f7443a08ebdc8dcc46f58b6c008959
jay <code.dongjunlee@gmail.com>
2026-08-13 10:37:24 +0900

    Time: 3 ms (80.93%), Space: 43.3 MB (82.83%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -2,21 +2,20 @@ class Solution {
     public int calPoints(String[] operations) {
         Deque<Integer> stack = new ArrayDeque<>();
         for (String op : operations) {
-            
             if ("C".equals(op)) {
                 stack.pop();
             } else if ("D".equals(op)) {
                 stack.push(stack.peek() * 2);
             } else if ("+".equals(op)) {
-                int top = stack.pop();
-                int half = stack.peek();
-                stack.push(top);
-                stack.push(top + half);
+                int last = stack.pop();
+                int prev = stack.peek();
+                stack.push(last);
+                stack.push(last + prev);
             } else {
                 stack.push(Integer.parseInt(op));
             }
         }
-                
+
         int result = 0;
         for (int n : stack) {
             result += n;
