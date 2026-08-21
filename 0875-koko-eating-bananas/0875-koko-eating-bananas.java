adaf56ed04b0f47abfb77dbcff283dc2fbacb06f
jay <code.dongjunlee@gmail.com>
2026-08-21 15:19:54 +0900

    Time: 13 ms (41.51%), Space: 48 MB (32.44%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,24 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
-        int right = Arrays.stream(piles).max().getAsInt();
-
-        while (left < right) {
-            int k = left + (right - left) / 2;
-
-            int eatingTime = 0;
-            for (int i = 0; i < piles.length; i++) {
-                eatingTime += piles[i] / k;
-                eatingTime += piles[i] % k != 0 ? 1 : 0;
-            }
-
-            if (eatingTime <= h) { // eatingTime이 같거나 작으면 오른쪽 범위 제거
-                right = k;
-            } else { // eatingTime이 더 크다면 k 증가
-                left = k + 1;
-            }
+        int right = 0;
+        for (int i = 0; i < piles.length; i++) {
+            right = Math.max(right, piles[i]);
         }
 
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            int hours = 0;
+            for (int pile : piles) {
+                hours += pile / mid;
+                if (pile % mid != 0) hours++;
+            }
+
+            if (hours <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
         return left;
     }
 }
+/**
+최소 k == 1시간에 먹을 수 있는 개수
+ */
