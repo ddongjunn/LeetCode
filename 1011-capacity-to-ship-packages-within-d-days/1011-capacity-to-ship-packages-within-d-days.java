4e1a542eddb1c21abdb85831a3aca2759a07910e
jay <code.dongjunlee@gmail.com>
2026-08-21 15:31:33 +0900

    Time: 15 ms (9.69%), Space: 49.8 MB (94.02%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -5,19 +5,18 @@ class Solution {
 
         while (left < right) {
             int mid = left + (right - left) / 2;
-
-            int count = 0;
-            int countDay = 1;
-            for (int i = 0; i < weights.length; i++) {
-                if (count + weights[i] > mid) {
-                    count = 0;
-                    countDay++;
-                }
-
-                count += weights[i];
-            }
             
-            if (countDay <= days) {
+            int d = 1;
+            int w = 0;
+            for (int weight : weights) {
+                w += weight;
+                if (mid < w) {
+                    d++;
+                    w = weight;
+                }
+            }
+
+            if (d <= days) {
                 right = mid;
             } else {
                 left = mid + 1;
@@ -27,10 +26,7 @@ class Solution {
     }
 }
 
-/*
-    left -> 최소 후보 (가장 무거운 상자)
-    right -> 최대 후보
-
-    days 안에 선박의 최소 적재 용량
-    - 정렬 X
-*/
+/**
+left == max 용량
+right == sum
+ */
