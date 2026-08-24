fbcf5269db004eca140a2e15f04e5bbc70454682
jay <code.dongjunlee@gmail.com>
2026-08-24 12:17:01 +0900

    Time: 2 ms (99.34%), Space: 46.9 MB (74.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,43 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        var map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = target - nums[i];
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
 
-            if (map.containsKey(num)) {
-                return new int[]{i, map.get(num)};
+        // Arrays.sort(nums);
+        // int left = 0;
+        // int right = nums.length - 1;
+        // while (left < right) {
+        //     int sum = nums[left] + nums[right];
+
+        //     if (sum == target) {
+        //         return new int[]{left, right};
+        //     }
+
+        //     if (sum < target) {
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+        // return new int[]{};
+        // 이 방법은 정렬하는 과정에서 문제에서 주어지 인덱스가 다르기 때문에 불가능 
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int t = target - nums[i];
+            
+            if (map.containsKey(t)) {
+                return new int[]{map.get(t), i};
             }
 
             map.put(nums[i], i);
         }
-
         return new int[]{};
     }
 }
